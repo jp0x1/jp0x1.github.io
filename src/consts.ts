f40420ded2cfae7a0656a69559943b7e52028a01
3dgyGamer <jakepark2908@gmail.com>
2025-06-04 18:35:32 -0400

    fixed up

--- src/consts.ts
+++ src/consts.ts
@@ -1,34 +1,44 @@
-export type Site = {
-  TITLE: string
-  DESCRIPTION: string
-  EMAIL: string
-  NUM_POSTS_ON_HOMEPAGE: number
-  POSTS_PER_PAGE: number
-  SITEURL: string
-}
-
-export type Link = {
-  href: string
-  label: string
-}
+import type { IconMap, SocialLink, Site } from '@/types'
 
 export const SITE: Site = {
-  TITLE: 'JP',
-  DESCRIPTION:
-    'This is jp blog',
-  EMAIL: 'jakepark2908@gmail.com',
-  NUM_POSTS_ON_HOMEPAGE: 2,
-  POSTS_PER_PAGE: 3,
-  SITEURL: 'https://jp0x1.github.io/',
+  title: 'jake',
+  description:
+    'jake blog.',
+  href: 'https://astro-erudite.vercel.app',
+  author: 'jake',
+  locale: 'en-US',
+  featuredPostCount: 2,
+  postsPerPage: 3,
 }
 
-export const NAV_LINKS: Link[] = [
-  { href: '/blog', label: 'blog' },
-  { href: '/tags', label: 'tags' },
+export const NAV_LINKS: SocialLink[] = [
+  {
+    href: '/blog',
+    label: 'blog',
+  },
 ]
 
-export const SOCIAL_LINKS: Link[] = [
-  { href: 'https://github.com/jp0x1', label: 'GitHub' },
-  { href: 'jakepark2908@gmail.com', label: 'Email' },
-  { href: '/rss.xml', label: 'RSS' },
+export const SOCIAL_LINKS: SocialLink[] = [
+  {
+    href: 'https://github.com/jp0x1',
+    label: 'GitHub',
+  },
+ 
+  {
+    href: 'mailto:jakepark2908@gmail.com',
+    label: 'Email',
+  },
+  {
+    href: '/rss.xml',
+    label: 'RSS',
+  },
 ]
+
+export const ICON_MAP: IconMap = {
+  Website: 'lucide:globe',
+  GitHub: 'lucide:github',
+  LinkedIn: 'lucide:linkedin',
+  Twitter: 'lucide:twitter',
+  Email: 'lucide:mail',
+  RSS: 'lucide:rss',
+}
